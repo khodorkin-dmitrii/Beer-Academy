afed8d5f13130a574525377db5656e099503a921
Khodorkin D <hodorkin.dmitriy@gmail.com>
2024-01-11 22:24:20 +0400

    bump db version

--- app/src/main/java/com/yavin/beeracademy/data/local/BeerDatabase.kt
+++ app/src/main/java/com/yavin/beeracademy/data/local/BeerDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [BeerEntity::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class BeerDatabase: RoomDatabase() {
