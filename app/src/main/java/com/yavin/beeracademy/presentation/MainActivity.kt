6d928202a4f6613d2887d45fc2e7f1fb76576173
Khodorkin D <hodorkin.dmitriy@gmail.com>
2024-01-12 20:05:42 +0400

    fix app background

--- app/src/main/java/com/yavin/beeracademy/presentation/MainActivity.kt
+++ app/src/main/java/com/yavin/beeracademy/presentation/MainActivity.kt
@@ -3,12 +3,17 @@ package com.yavin.beeracademy.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.yavin.beeracademy.Navigation
+import com.yavin.beeracademy.R
 import com.yavin.beeracademy.ui.theme.BeerAcademyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,7 +32,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Navigation()
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(
+                                if (isSystemInDarkTheme())
+                                    colorResource(id = R.color.background_night)
+                                else
+                                    colorResource(id = R.color.background_day)
+                            )
+                    ) {
+                        Navigation()
+                    }
                 }
             }
         }
